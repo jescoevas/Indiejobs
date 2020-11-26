4ac4eecfefc41ea884fec68f908494096843da9a
Jesus Coello <jescoevas@hotmail.com>
2020-11-26 20:00:32 +0100

    Loading en login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
@@ -14,7 +15,8 @@ export class LoginPage {
   emailIncorrecto:boolean = false
   passwordIncorrecta:boolean = false
 
-  constructor(private router:Router, private fb:FormBuilder, private usuarioService:UsuarioService) {
+  constructor(private router:Router, private fb:FormBuilder, private usuarioService:UsuarioService,
+    private loading:LoadingController) {
     this.form = this.fb.group({
       email:['', [Validators.required]],
       password:['', [Validators.required]]
@@ -29,6 +31,13 @@ export class LoginPage {
     }
 
   async login(){
+    const loading = await this.loading.create({
+      // cssClass: 'my-custom-class',
+      message: 'Por favor, espere...',
+      // duration: 2000
+    });
+    await loading.present();
+
     const msg = await this.usuarioService.login(this.form.value)
     if(msg === 'No se ha encontrado el usuario'){
       this.emailIncorrecto = true
@@ -37,6 +46,7 @@ export class LoginPage {
     }else{
       this.router.navigateByUrl('/')
     }
+    await loading.dismiss()
   }
 
 }
